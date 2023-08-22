3335f6b795b168f6ed4ff9c78a34eda24fd96bc3
chiefJurist <anthonynnanna004@gmail.com>
2023-08-22 08:56:58 +0100

    correction

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Jobs from '../views/jobs/Jobs.vue'
-import JobDetails from '../views/jobs/JobDetails.vue'
+import JobDetails from '../views/jobs/jobDetails.vue'
 import NotFound from '../views/NotFound.vue'
 
 const routes = [
